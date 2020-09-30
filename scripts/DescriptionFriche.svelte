8282eba48b7f1e58a8e6adb448aee1a60a880d3b
David Bruant <davidbruant@protonmail.com>
2020-09-30 17:18:44 +0200

    Squelette (#2)
    * Squelette
    
    * Identification de la friche et occupation
    
    * Dernière activité connue
    
    * Nature de l'artificialisation et archéologie
    
    * Ajout champs Caractéristiques du bâti
    
    * Champs Connaissance de la pollution
    
    * Champs Urbanisme
    
    * tabs style
    
    * Champs Risques et caractéristiques naturelles
    
    * Champs Transport et réseaux
    
    * tabs style, le retour de la vengeance
    
    * Champs Economie et marché
    
    * Champs Le projet sur le site

--- scripts/DescriptionFriche.svelte
+++ scripts/DescriptionFriche.svelte
@@ -1,26 +1,289 @@
 
-<section class="description-friche">
+<article class="description-friche">
 	<h1>Description d'une friche</h1>
-	{#each fricheEntries as [question, answer]}
-	<h2>{question}</h2>
-	{answer}
-	{/each}
-</section>
+	
+	<div class="row">
+		<section class="identification">
+			<h2>{descriptionFriche.name}</h2>
+			<h3>Adresse du site</h3>
+			{descriptionFriche.address}
+			<h3>N° INSEE commune</h3>
+			{descriptionFriche.communeINSEECode}
+			<h3>Complément de localisation si besoin</h3>
+			{descriptionFriche.addressComplement}
+			<h3>Parcelles cadastrales</h3>
+			{descriptionFriche.parcellesCadastrales}
+			<h3>Périmètre</h3>
+			{descriptionFriche.perimeterDescription}
+			<h3>Surface</h3>
+			{descriptionFriche.area}m²
+		</section>
+		<section class="tabs">
+			<a href="#occupation">Occupation actuelle et propriété sur le site</a>
+			<a href="#former-activity">Cessation d’activité</a>
+			<a href="#artificialisation-archéologie">Nature de l'artificialisation et archéologie</a>
+			<a href="#caractéristiques-du-bâti">Caractéristiques du bâti</a>
+			<a href="#connaissance-de-la-pollution">Connaissance de la pollution</a>
+			<a href="#urbanisme">Urbanisme</a>
+			<a href="#nature">Risques et caractéristiques naturelles</a>
+			<a href="#économie-et-marché">Économie et marché</a>
+			<a href="#projet">Le projet sur le site</a>
+		</section>
+	</div>
+
+	<section class="tab-content">
+		<section id="occupation">
+			<h2>Occupation actuelle et propriété sur le site</h2>
+			<h3>Le site est-il occupé ?</h3>
+			{descriptionFriche.occupancy}
+			<h3>Description libre de l'occupation éventuelle</h3>
+			{descriptionFriche.occupancyDescription}
+			<h3>Occupants actuels</h3>
+			{descriptionFriche.currentOccupents}
+			<h3>Propriétaires publics actuels</h3>
+			{descriptionFriche.currentPublicOwners}
+			<h3>Propriétaires privés actuels</h3>
+			{descriptionFriche.currentPrivateOwners}
+			<h3>Existe-t-il des droits liés à des propriétaires inconnus ?</h3>
+			{descriptionFriche.ownersRelatedRights}
+		</section>
+		<section id="former-activity">
+			<h2>Cessation d’activité</h2>
+
+			<h3>Site IPCE</h3>
+			{descriptionFriche.IPCEStatus}
+			<h3>Vacant depuis</h3>
+			{descriptionFriche.vacantSinceDate}
+			<h3>Ancien occupant</h3>
+			{descriptionFriche.formerOccupantName}
+			<h3>Quelle est l'ancienne activité du site ?</h3>
+			{descriptionFriche.formerActivity}
+			<h3>Détails sur l'activité précédente</h3>
+			{descriptionFriche.detailedFormerActivity}
+			<h3>Site mis en sécurité</h3>
+			{descriptionFriche.safetyStatus}
+			<h3>Obligations du dernier occupant</h3>
+			{descriptionFriche.formerOperatorRequirementsFulfillmentStatus}
+
+		</section>
+		<section id="artificialisation-archéologie">
+			<h2>Nature de l'artificialisation et archéologie</h2>
+
+			<h3>Bâti</h3>
+			{descriptionFriche.built}
+			<h3>Imperméabilisation estimée</h3>
+			{descriptionFriche.waterproofingRate}%
+			<h3>Types de sols majoritaires</h3>
+			{descriptionFriche.soilTypes}
+			<h3>Fouilles archéologiques</h3>
+			{descriptionFriche.archelogyObligation}
+			<h3>Est-ce qu'il y a déjà eu des fouilles sur ce site ?</h3>
+			{descriptionFriche.archelogyObligationStatus}
+		</section>
+		<section id="caractéristiques-du-bâti">
+			<h2>Caractéristiques du bâti</h2>
+
+			<h3>Patrimoine architectural</h3>
+			{descriptionFriche.patrimonialStatus}
+			
+			<h3>Surface de plancher</h3>
+			{descriptionFriche.floorArea}%
+
+			<h3>Nombre de niveaux</h3>
+			{descriptionFriche.floors}
+
+			<h3>Etat du bâti</h3>
+			{descriptionFriche.builtQuality}
+
+			<h3>Âge estimé</h3>
+			{descriptionFriche.builtAge}
+		</section>
+		<section id="connaissance-de-la-pollution">
+			<h2>Connaissance de la pollution</h2>
+
+			<h3>Si ce site est ICPE, avez-vous pu récupérer les éléments de connaissance de la pollution auprès de la DREAL ?</h3>
+			{descriptionFriche.pollutionInfoFromDREAL}
+			
+			<h3>Pollutions</h3>
+			<h4>Sols</h4>
+			Pollution : {descriptionFriche.pollution.soil.level}<br>
+			Niveau de confiance : {descriptionFriche.pollution.soil.confidence}
+			<h4>Eaux</h4>
+			Pollution : {descriptionFriche.pollution.water.level}<br>
+			Niveau de confiance : {descriptionFriche.pollution.water.confidence}
+			<h4>Bâtiments</h4>
+			Pollution : {descriptionFriche.pollution.buildings.level}<br>
+			Niveau de confiance : {descriptionFriche.pollution.buildings.confidence}
+
+			<h3>États des recherches</h3>
+			{descriptionFriche.pollutionResearch}
+
+			<h3>Documents sur la pollution</h3>
+			{descriptionFriche.pollutionDocuments}
+
+		</section>
+		<section id="urbanisme">
+			<h2>Urbanisme</h2>
+
+			<h3>Zonage PLU(i)</h3>
+			{descriptionFriche.PLUZone}
+			
+			<h3>Une modification du PLU(i) est-elle prévue ?</h3>
+			{descriptionFriche.plannedPLUModification}
+
+			<h3>Contraintes d'urbanisation</h3>
+			{descriptionFriche.urbanisationConstraints}
+
+			<h3>Contexte SCoT, SRADDET</h3>
+			{descriptionFriche.urbanisationLegalDocumentConstraints}
+
+			<h3>Contraintes foncières</h3>
+			{descriptionFriche.propertyConstraints}
+
+			<h3>Sols alentour</h3>
+			{descriptionFriche.surroundingSoilUsage}
+
+			<h3>Tissu urbain alentour</h3>
+			{descriptionFriche.surroundingUrbanContext}
+		</section>
+		<section id="nature">
+			<h2>Risques et caractéristiques naturelles</h2>
+
+			<h3>Zone PPR</h3>
+			{descriptionFriche.risks}
+			
+			<h3>Précisions sur le niveau d'aléa et la part du site touché</h3>
+			{descriptionFriche.anticipatedRisks}
+
+			<h3>Richesse écologique</h3>
+			{descriptionFriche.biodiversity}
+
+			<h3>Précisions concernant la richesse écologique du site</h3>
+			{descriptionFriche.biodiversityDetails}
+
+			<h3>Contraintes naturelles</h3>
+			{descriptionFriche.naturalConstraints}
+
+		</section>
+		<section id="transport-réseaux">
+			<h2>Transport et réseaux</h2>
+
+			<h3>Desserte routière</h3>
+			{descriptionFriche.roadConnectivity}
+
+			<h3>Gare la plus proche</h3>
+			{descriptionFriche.closestRailroadStation}km
+
+			<h3>Assainissement</h3>
+			{descriptionFriche.santitationConnectivity}
+
+			<h3>Alimentation électrique</h3>
+			{descriptionFriche.energyConnectivity}
+
+			<h3>Connectivité</h3>
+			{descriptionFriche.internetConnectivity}
+
+		</section>
+		<section id="économie-et-marché">
+			<h2>Économie et marché</h2>
+
+			<h3>Taxe foncière actuelle</h3>
+			{descriptionFriche.currentPropertyTax}€
+
+			<h3>Autres apports financiers éventuels</h3>
+			{descriptionFriche.currentSiteIncomes}
+
+			<h3>Prix foncier constructible alentour</h3>
+			{descriptionFriche.averageSurroundingBuildableCost}€/m²
+
+			<h3>Prix foncier bâti alentour</h3>
+			{descriptionFriche.averageSurroundingBuiltCost}€/m²
+
+		</section>
+		<section id="projet">
+			<h2>Le projet sur le site</h2>
+
+			<h3>Projet en cours de réalisation</h3>
+			{descriptionFriche.currentProject}
+
+			<h3>Détails</h3>
+			{descriptionFriche.currentProjectDetails}
+
+			<h3>Projet envisagé ou en réflexion</h3>
+			{descriptionFriche.projectPlans}
+
+			<h3>Usages envisagés</h3>
+			{descriptionFriche.plannedUsages}
+
+		</section>
+	</section>
+</article>
 
 <style>
-	.description-friche h2{
-		font-size: 1.8rem;
-		text-decoration-line: underline;
-		text-decoration-style: solid;
-		text-decoration-color: #CCC;
+	.description-friche{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 
+	h2{
+		font-size: 2rem;
+		color: #008080;
+	}
+
+	h3{
+		font-size: 1.8rem;
+	}
+
+	.row{
+		width: 100%;
+		margin-bottom: 1em;
+		display: flex;
+		flex-direction: row;
+	}
+	.identification{
+		flex: 1;
+	}
+	.tabs{
+		flex: 1;
+	}
+
+	.identification, .tabs, .tab-content{
+		border: 1px solid #008080; /* TOUTDOUX créer une variable */
+		border-radius: 2em;
+		padding: 1em;
+	}
+
+	.tabs{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+	}
+
+	.tabs > *{
+		font-size: 1.1em;
+		padding: 0.5em;
+	}
+
+	.tab-content{
+		width: 100%;
+	}
+	
+	.tab-content > section{
+		display: none;
+	}
+	
+	.tab-content > section:target{
+		display: block;
+	}
 </style>
 
 <script>
+	// @ts-check
+
+	/**
+	* @type {import("./main.js").Friche}
+	*/
 	export let descriptionFriche;
 
-	$: fricheEntries = Object.entries(descriptionFriche).filter(([key]) => {
-		return !['ID', 'Heure de début', 'Heure de fin', 'Adresse de messagerie', 'Nom'].includes(key)
-	})
 </script>
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,192 @@
+// @ts-check
+
 import {csv} from 'd3-fetch'
 
 //import App from './App.svelte';
 import DescriptionFriche from './DescriptionFriche.svelte';
 
-csv('./data/descriptions-friches.csv')
-.then(descriptionsFriches => {
-	console.log('descriptionsFriches', descriptionsFriches)
+/**
+ * @typedef {object} Person
+ * @property {string} id
+ * @property {string} name
+ * @property {string} emailAddress
+ */
 
+/**
+ * @typedef {object} Friche
+ * Identification
+ * @property {string} [id]
+ * @property {string} name
+ * @property {string} address
+ * @property {string} addressComplement
+ * @property {string} communeINSEECode
+ * @property {string} [parcellesCadastrales]
+ * @property {string} [perimeterDescription]
+ * @property {string} [area]
+ * Occupation actuelle et propriété du site
+ * @property {string} occupancy - Le site est-il occupé ? Site totalement vacant / Occupé en partie de manière temporaire / ... / Pas d'information
+ * @property {string} occupancyDescription
+ * @property {string} currentOccupents
+ * @property {string} currentPublicOwners
+ * @property {string} currentPrivateOwners
+ * @property {string} ownersRelatedRights 
+ * Cessation d'activité
+ * @property {string} IPCEStatus
+ * @property {string} vacantSinceDate
+ * @property {string} formerOccupantName
+ * @property {string} formerActivity
+ * @property {string} detailedFormerActivity
+ * @property {string} safetyStatus
+ * @property {string} formerOperatorRequirementsFulfillmentStatus
+ * Nature de l'artificialisation et archéologie
+ * @property {string} built
+ * @property {string} waterproofingRate
+ * @property {string} soilTypes
+ * @property {string} archelogyObligation
+ * @property {string} archelogyObligationStatus
+ * Caractéristiques du bâti
+ * @property {string} patrimonialStatus
+ * @property {string} floorArea
+ * @property {string} floors
+ * @property {string} builtQuality
+ * @property {string} builtAge
+ * Connaissance de la pollution
+ * @property {string} pollutionInfoFromDREAL
+ * @property {object} pollution
+ * @property {object} pollution.soil
+ * @property {object} pollution.water
+ * @property {object} pollution.buildings
+ * @property {string} pollutionResearch
+ * @property {string} pollutionDocuments
+ * Urbanisme
+ * @property {string} PLUZone
+ * @property {string} plannedPLUModification
+ * @property {string} urbanisationConstraints
+ * @property {string} urbanisationLegalDocumentConstraints
+ * @property {string} propertyConstraints
+ * @property {string} surroundingSoilUsage
+ * @property {string} surroundingUrbanContext
+ * Risques et caractéristiques naturelles
+ * @property {string} risks
+ * @property {string} anticipatedRisks
+ * @property {string} biodiversity
+ * @property {string} biodiversityDetails
+ * @property {string} naturalConstraints
+ * Transport et réseaux
+ * @property {string} roadConnectivity
+ * @property {string} closestRailroadStation
+ * @property {string} santitationConnectivity
+ * @property {string} energyConnectivity
+ * @property {string} internetConnectivity
+ * Economie et marché
+ * @property {string} currentPropertyTax
+ * @property {string} currentSiteIncomes
+ * @property {string} averageSurroundingBuildableCost
+ * @property {string} averageSurroundingBuiltCost
+ * Le projet sur le site
+ * @property {string} currentProject
+ * @property {string} currentProjectDetails
+ * @property {string} projectPlans
+ * @property {string} plannedUsages
+ */
+
+/**
+ * @param {unknown} csvObj
+ * @return {Friche} 
+ */
+function makeFriche(csvObj){
+	return {
+		name: csvObj[`Nom du site`],
+		address: csvObj[`Adresse du site`],
+		communeINSEECode: csvObj[`Code INSEE de la commune où se situe le site`],
+		addressComplement: csvObj[`Complément de localisation si besoin`],
+		parcellesCadastrales: csvObj['Numéros des parcelles cadastrales concernées'],
+		perimeterDescription: csvObj[`Description du périmètre de la friche (en attendant qu'on vous invite à le dessiner directement !)`],
+		area: csvObj[`Une idée de la surface globale (m²) ?`],
+		
+		occupancy: csvObj[`Le site est-il occupé ?`],
+		occupancyDescription: csvObj[`Description libre de l'occupation éventuelle (normalement question conditionnelle)`],
+		currentOccupents: csvObj[`Nom des éventuels occupants actuels et activité`],
+		currentPublicOwners: csvObj[`Listez les propriétaires publics actuels et leurs droits`],
+		currentPrivateOwners: csvObj[`Listez les propriétaires privés actuels et leurs droits`],
+		ownersRelatedRights: csvObj[`Existe-t-il des droits liés à des propriétaires inconnus ?`],
+		
+		IPCEStatus: csvObj[`Le site est-il classé ICPE ?`],
+		vacantSinceDate: csvObj[`Le site est vacant (ou partiellement vacant) depuis l'année`],
+		formerOccupantName: csvObj[`Nom de l'ancien occupant du site`],
+		formerActivity: csvObj[`Quelle est l'ancienne activité du site ?`],
+		detailedFormerActivity: csvObj[`Décrire plus finement l'ancienne activité si possible`],
+		safetyStatus: csvObj[`Le site est-il mis en sécurité ?`],
+		formerOperatorRequirementsFulfillmentStatus: csvObj[`Le dernier exploitant a-t-il été purgé de ses obligations ?`],
+		
+		built: csvObj[`Le site est-il bâti ?`],
+		waterproofingRate: csvObj[`Quel taux d'imperméabilisation des sols estimez-vous ?`],
+		soilTypes: csvObj[`Sur le site, les sols présents sont de type`],
+		archelogyObligation: csvObj[`Est-ce que le site est soumis à obligation de fouille ?`],
+		archelogyObligationStatus: csvObj[`Est-ce qu'il y a déjà eu des fouilles sur ce site ?`],
+
+		patrimonialStatus: csvObj[`Le bâti a-t-il un aspect patrimonial ?`],
+		floorArea: csvObj[`Surface de plancher (ordre de grandeur)`],
+		floors: csvObj[`Nombre de niveaux`],
+		builtQuality: csvObj[`Etat global du bâti`],
+		builtAge: csvObj[`Age du bâti (approximatif)`],
+
+		pollutionInfoFromDREAL: csvObj[`Si ce site est ICPE, avez-vous pu récupérer les éléments de connaissance de la pollution auprès de la DREAL ?`],
+		pollution: {
+			soil: {
+				confidence: csvObj['Les sols'],
+				level: csvObj['Les sols2'],
+			},
+			water: {
+				confidence: csvObj['Les eaux'],
+				level: csvObj['Les eaux2'],
+			},
+			buildings: {
+				confidence: csvObj['Les bâtiments'],
+				level: csvObj['Les bâtiments2'],
+			},
+		},
+		pollutionResearch: csvObj[`En cas de suspicion de pollution, indiquez ici l'état de vos recherches`],
+		pollutionDocuments: csvObj[`En cas de suspicion ou certitude de pollution, disposez-vous de documents ?`],
+
+		PLUZone: csvObj['Quel zonage PLU(i) concerne actuellement le site ?'],
+		plannedPLUModification: csvObj['Une modification du PLU(i) est-elle prévue ?'],
+		urbanisationConstraints: csvObj[`Le site est-il concerné par un périmètre de projet, ZAC, autre contrainte d'urbanisation ?`],
+		urbanisationLegalDocumentConstraints: csvObj[`Les documents d'urbanisme en amont (SCoT, SRADDET) traitent-ils de la réduction de l'artificialisation des terres naturelles et agricoles ? Si oui, précisez l'obligation/recommandation`],
+		propertyConstraints: csvObj[`Le site est-il soumis à des contraintes foncières particulières ?`],
+		surroundingSoilUsage: csvObj[`Types d'usage du sol aux alentours`],
+		surroundingUrbanContext: csvObj[`Type de tissu urbain aux alentours`],
+
+		risks: csvObj[`Le site est-il concerné par un zonage de plan de prévention des risques`],
+		anticipatedRisks: csvObj[`Si oui, indiquez ici les précisions sur le niveau d'aléa et la part du site touché`],
+		biodiversity: csvObj[`Que connaissez-vous de la richesse écologique du site ?`],
+		biodiversityDetails: csvObj[`Indiquez ici les précisions concernant la richesse écologique du site`],
+		naturalConstraints:  csvObj[`Le site est-il soumis à des contraintes ou servitudes naturelles ?`],
+
+		roadConnectivity:  csvObj[`Quel est le niveau de desserte routière ?`],
+		closestRailroadStation:  csvObj[`Quelle est la distance à la gare la plus proche ? (en km)`],
+		santitationConnectivity:  csvObj[`Quelle desserte par les réseaux d'assainissement ?`],
+		energyConnectivity:  csvObj[`Quel niveau d'alimentation énergétique est possible ?`],
+		internetConnectivity:  csvObj[`Quel état de  la connectivité ?`],
+
+		currentPropertyTax: csvObj[`Montant approximatif de la taxe foncière générée actuellement par le site`],
+		currentSiteIncomes: csvObj[`Quels autres apports financiers le site génère-t-il éventuellement ?`],
+		averageSurroundingBuildableCost: csvObj[`Coût moyen du terrain constructible dans un rayon de 200 m autour du site (euros/m²)`],
+		averageSurroundingBuiltCost: csvObj[`Coût moyen du foncier bâti dans un rayon de 200 m autour du site (euros/m²)`],
+
+		currentProject: csvObj[`Y a-t-il déjà un projet en cours de réalisation sur ce site ?`],
+		currentProjectDetails: csvObj[`Précisions sur cet éventuel projet en cours de réalisation`],
+		projectPlans: csvObj[`Avez-vous un projet envisagé/en réflexion ?`],
+		plannedUsages: csvObj[`Le projet que j'envisage concerne des usages`],
+	}
+}
+
+
+csv('./data/descriptions-friches.csv')
+.then(descriptionsFrichesCSV => {
+	const descriptionsFriches = descriptionsFrichesCSV.map(makeFriche)
+
+	console.log('descriptionsFriches', descriptionsFriches)
 	
 	const app = new DescriptionFriche({
 		target: document.querySelector('.svelte-main'),
